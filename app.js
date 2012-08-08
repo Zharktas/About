f7354a5f071743060bbebab7658acf67bcf175f8
Jari Voutilainen <jari.voutilainen@iki.fi>
2012-08-08 10:40:22 +0300

    Change templating engine to jade since ejs doesn't support layouts yet.

--- app.js
+++ app.js
@@ -5,20 +5,21 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , http = require('http');
+  , http = require('http')
+  , path = require('path');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 9001);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
