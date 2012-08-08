70909c909846e107a2693164548787773697797e
Jari Voutilainen <jari.voutilainen@iki.fi>
2012-08-08 12:16:05 +0300

    Add some routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,14 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Zharktas' });
+};
+
+exports.projects = function(req, res){
+	res.render('projects', {title: "Projects"});
+};
+
+
+exports.cv = function(req, res){
+	res.render('cv', {title: "CV"});
 };
